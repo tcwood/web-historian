40a020cd9746577cbb6621a332591baa57e8329d
tcwood <travis.cwood@gmail.com>
2016-10-05 15:15:34 -0700

    Get sends back Index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //Get the html of the index.html into a variable
+  if (req.method === 'GET' || req.method === 'OPTIONS') {
+
+
+
+    var index = archive.paths.siteAssets + '/index.html';
+
+    //Read the contents of index.html
+    fs.readFile(index, (err, data) => {
+      res.writeHead(200, httpHelpers.headers);
+      res.end(data);
+    });
+    
+
+
+  }
 };
