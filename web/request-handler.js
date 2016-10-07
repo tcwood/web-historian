e02a591ccce864f41757632fdf77788b30ff9aac
tcwood <travis.cwood@gmail.com>
2016-10-06 21:18:01 -0700

    Complete serveassets function & redirect for blank url

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,11 +6,12 @@ var httpHelpers = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
   if (req.method === 'GET' || req.method === 'OPTIONS') {
+    console.log('getgetgetrequest', req.url);
     archive.isUrlArchived(req.url, function(exists) {
       if (exists) {
         var index;
         if (req.url === '/' || req.url === '/index.html') {
-          index = archive.paths.siteAssets + '/' + 'index.html';
+          index = archive.paths.siteAssets + '/index.html';
         } else {
           index = archive.paths.archivedSites + '/' + req.url;
         }
@@ -25,6 +26,12 @@ exports.handleRequest = function (req, res) {
       }
     });
   } else if (req.method === 'POST') {
+    //check if we have file
+      //if we do,
+        // write to headers 302 and object (that has key Location, value = full URL want to be directed to)
+      //if we don't
+        //give loading html
+
     var urlBuffer = '';
     
     req.on('data', function(data) {
@@ -33,13 +40,28 @@ exports.handleRequest = function (req, res) {
 
     req.on('end', function() {
       var url = urlBuffer.slice(4);
-      fs.appendFile(archive.paths.list, url + '\n', function (err) {
-        if (!err) {
-          res.writeHead(302, httpHelpers.headers);
-          res.end();
-        }     
-      });
 
+      if (url === '') {
+        httpHelpers.serveAssets(res, archive.paths.siteAssets + '/index.html', function(data) {
+          res.writeHead(200, httpHelpers.headers);
+          res.end(data);
+        });
+      }
+
+      archive.isUrlArchived(url, function(exists) {
+        if (exists) {
+          res.writeHead(302, {Location: 'http://127.0.0.1:8080/' + url});
+          res.end();
+        } else {
+          fs.appendFile(archive.paths.list, url + '\n', function (err) {
+            if (!err) {
+              console.log('POSTPOTSPPOST', url);
+              res.writeHead(201, {Location: 'http://127.0.0.1:8080/loading.html'});
+              res.end();
+            }
+          });
+        }
+      });
     });
   }
 };
